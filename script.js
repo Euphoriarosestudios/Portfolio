c1c4893da082ac4793274a7992502bdca8c3e31c
Euphoriarosestudios <153433097+Euphoriarosestudios@users.noreply.github.com>
2024-11-17 12:21:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let currentSlide = 1; // Default slide is Digital Painting
+let currentSlide = 0; // Default slide is Digital Painting
 
 window.onload = function() {
     changeSlide(1); // Ensure the first slide (Digital Painting) is shown on load
@@ -8,6 +8,11 @@ function changeSlide(slideIndex) {
     const slides = document.querySelectorAll('.slide');
     const aboutMeSection = document.getElementById('aboutMe');
     const characterDetailsSection = document.getElementById('characterDetails');
+    const  PortfolioSection = document.getElementById('portfolio');
+    const  paintingsSection = document.getElementById('paintings');
+
+                    
+
 
     currentSlide = slideIndex;
 
@@ -19,12 +24,23 @@ function changeSlide(slideIndex) {
     if (currentSlide === 0) {
         aboutMeSection.style.display = 'none'; 
         characterDetailsSection.style.display = 'block'; /* Show when Character Design is active */
+        PortfolioSection.style.display = 'none';
+        paintingsSection.style.display = 'block';
+
+
     } else if (currentSlide === 1) {
         aboutMeSection.style.display = 'block'; 
         characterDetailsSection.style.display = 'none'; 
-    } else {
-        aboutMeSection.style.display = 'none'; 
-        characterDetailsSection.style.display = 'none'; 
+        PortfolioSection.style.display = 'block';
+        paintingsSection.style.display = 'block';
+
+
+    } else if (currentSlide === 3) { 
+        aboutMeSection.style.display = 'block'; 
+        characterDetailsSection.style.display = 'block'; 
+        PortfolioSection.style.display = 'block';
+        paintingsSection.style.display = 'none';
+
     }
 }
 function showIllustrations() {
@@ -35,4 +51,3 @@ function showIllustrations() {
     // Show the "Illustrations" section
     document.getElementById('illustrationsSection').style.display = 'block';
 }
-
